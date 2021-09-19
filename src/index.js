7f737dc27745aa82242017999d03e0471213bc03
Omar Alazaizeh <omx302@gmail.com>
2021-09-19 14:48:40 +0300

    converted

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import App from "./app";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
